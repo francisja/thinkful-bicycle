dca94ae7b60ea9e31880fd03967731725b2f9964
Francis Alano <francis.alano@gmail.com>
2016-09-12 01:27:11 -0700

    update

--- bicycle_industry.py
+++ bicycle_industry.py
@@ -1,163 +1,232 @@
+import random
+
 class Bicycle(object):
-    def __init__(self, bike_name, weight, production_cost, margin):
+    def __init__(self, bike_name, weight, production_cost):
         self._bike_name = bike_name
         self._weight = weight
         self._production_cost = production_cost
     @property
     def bike_name(self):
+        """
+        returns the bike name with the @property decorator
+        """
         return self._bike_name
     @bike_name.setter
     def bike_name(self, new_bike_name):
+        """
+        sets the new bike name with the @setter decorator as long as the new bike name is a string
+        """
         if isinstance(new_bike_name, str):
             self._bike_name = new_bike_name
         else:
             raise ValueError("Bike name must be a string")
     @property
     def weight(self):
+        """
+        returns the weight of the bike with the @property decorator
+        """
         return self._weight
     @weight.setter
     def weight(self, new_weight):
+        """
+        sets the new weight with the @setter decorator as long as the new weight is an integer
+        """
         if isinstance(new_weight, int):
             self._weight = new_weight
         else:
             raise ValueError("Weight should be changed to an integer.")
     @property
     def production_cost(self):
+        """
+        returns the production cost of the bike with the @property decorator
+        """
         return self._production_cost
     @production_cost.setter
     def production_cost(self, new_production_cost):
+        """
+        sets the new production cost with the @setter decorator as long as the new cost is an integer
+        """
         if isinstance(new_production_cost, int):
             self.production_cost = new_production_cost
         else:
             raise ValueError("Production cost must be an integer.")
     def get_marginalized_cost(self, margin):
+        """
+        takes the production cost and multiplies it by 1 + the marginal increase of the store
+        """
         return self.production_cost * (1 + margin)
 
-
-
 class Bikeshop(object):
-    def __init__(self, shop_name, shop_inventory, sell_price, profit):
+    def __init__(self, shop_name, shop_inventory, margin, sell_price, profit):
         self._shop_name = shop_name
         self._shop_inventory = shop_inventory
+        self._margin = margin
         self._sell_price = sell_price
         self._profit = profit
     @property
     def shop_name(self):
+        """
+        returns shop name with the @property decorator
+        """
         return self._shop_name
     @shop_name.setter
     def shop_name(self, new_shop_name):
+        """
+        sets the new shop name with the @setter decorator as long as the new name is a string
+        """
         if isinstance(new_shop_name, str):
             self._shop_name = new_shop_name
         else:
             raise ValueError("Shop name needs to be a string.")
     @property
     def shop_inventory(self):
+        """
+        returns shop inventory with the @property decorator
+        """
         return self._shop_inventory
     @shop_inventory.setter
     def shop_inventory(self, new_shop_inventory):
+        """
+        sets the new shop inventory with the @setter decorator as long as the new inventory is a dictionary
+        """
         if isinstance(new_shop_inventory, dict):
             self._shop_inventory.update(new_shop_inventory)
         else:
             raise KeyError("Shop inventory should be a dictionary.")
     @property
-    def sell_price(self):
-        return self._sell_price
-    @sell_price.setter
-    def sell_price(self, new_sell_price):
-        if isinstance(new_sell_price, int):
-            self._sell_price = new_sell_price
+    def margin(self):
+        """
+        returns the margin with the @property decorator
+        """
+        return self._margin
+    @margin.setter
+    def margin(self, new_margin):
+        """
+        sets the new margin with the @setter decorator as long as the new margin is an integer
+        """
+        if isinstance(new_margin, int):
+            self._margin = new_margin
         else:
-            raise ValueError("Sell price must be an integer.")
+            raise ValueError("Margin needs to be an integer.")
+    @property
+    def sell_price(self):
+        """
+        returns the selling price with the @property decorator by creating a dictionary of the bike name with the marginalized cost of the bike
+        """
+        prices = {}
+        for bike in bikes:
+            selling_price = {bike.bike_name: bike.get_marginalized_cost(self.margin)}
+            prices.update(selling_price)
+        return prices
     @property
     def profit(self):
         return self._profit
-    @profit.setter
-    def profit(self, new_profit):
-        if isinstance(new_profit, int):
-            self._profit = new_profit
+    def sell_bike(self, new_bicycle):
+        if isinstance(new_bicycle, Bicycle):
+            new_bike_name = new_bicycle.bike_name
+            cost = self.sell_price[new_bike_name]
+            pcost = new_bicycle.production_cost
+            self._profit = cost - pcost
         else:
-            raise ValueError("Profit must be an integer.")
+            raise ValueError("The new bike needs to be from the Bicycle class")
+
+
+
 
 class Customers(object):
-    def __init__(self, customer_name, funds, buy):
+    def __init__(self, customer_name, funds, bicycle):
         self._customer_name = customer_name
         self._funds = funds
-        self._buy = buy
+        self._bicycle = bicycle
     @property
     def customer_name(self):
+        """
+        returns the customer name with the @property decorator
+        """
         return self._customer_name
     @customer_name.setter
     def customer_name(self, new_customer_name):
+        """
+        sets the new customer name with the @setter decorator as long new name is a string
+        """
         if isinstance(new_customer_name, str):
             self._customer_name = new_customer_name
         else:
             raise ValueError("Customer name should be a string.")
     @property
     def funds(self):
+        """
+        returns the customer's funds with the @property decorator
+        """
         return self._funds
     @funds.setter
     def funds(self, new_funds):
+        """
+        sets the new customer's funds with the @setter decorator as long new name is an integer
+        """
         if isinstance(new_funds, int):
             self._funds = new_funds
         else:
             raise ValueError("Funds must be an integer.")
     @property
-    def buy(self):
-        return self._buy
-    @buy.setter
-    def buy(self, new_buy):
-        if isinstance(new_buy, str):
-            self._buy = new_buy
+    def bicycle(self):
+        return self._bicycle
+    def buy_bicycle(self, new_bicycle, bike_shop):
+        if isinstance(new_bicycle, Bicycle) and isinstance(bike_shop, Bikeshop):
+            self._bicycle = new_bicycle
+            new_bike_name = new_bicycle.bike_name
+            cost = bike_shop.sell_price[new_bike_name]
+            self._funds = self._funds - cost
         else:
-            ValueError("The buy must be a string")
+            raise ValueError("New bike needs to be a bike in the Bicycle class.")
+    def get_affordable_bikes(self, shops):
+        """
+        returns a list of affordable bikes for the customer by comparing their funds with the selling price of the shop with the @property decorator
+        """
+        affordable_bikes = []
+        for shop in shops:
+            for model, price in shop.sell_price.items():
+                if self.funds >= price:
+                    affordable_bikes.append(model)
+        return affordable_bikes
+
 
 b1 = Bicycle("Gary Turner Mountain Bike", 45, 145)
 b2 = Bicycle("Big Shot Fixed Bike", 25, 125)
-b3 = Bicycle("New Trek Racing Bike", 20, 275)
+b3 = Bicycle("New Trek Racing Bike", 20, 475)
 b4 = Bicycle("Odyssey BMX Bike", 40, 250)
-b5 = Bicycle("Schwinn Cruiser Bike", 55, 175)
-b6 = Bicycle("VoltBike Electric Bike", 85, 400)
+b5 = Bicycle("Schwinn Cruiser Bike", 55, 375)
+b6 = Bicycle("VoltBike Electric Bike", 85, 700)
 bikes = [b1, b2, b3, b4, b5, b6]
 
-customer1 = Customers("Joe", 200, 0)
-customer2 = Customers("Michelle", 500, 0)
-customer3 = Customers("Taylor", 1000, 0)
+c1 = Customers("Joe", 200, None)
+c2 = Customers("Michelle", 500, None)
+c3 = Customers("Taylor", 1000, None)
+customer_list = [c1, c2, c3]
 
+s1 = Bikeshop("Bikeland", bikes, 0.2, {}, 0)
+my_shops = [s1]
 
+for customer in customer_list:
+    print(customer.get_affordable_bikes(my_shops))
+c1.buy_bicycle(b1, s1)
+print(c1.bicycle.bike_name)
+print(c1.funds)
+s1.sell_bike(b1)
+print(s1.profit)
+##for bike in bikes:
+##    print(bike.production_cost)
+##for customer in customer_list:
+##    print( "With {0}'s budget of ${1}, the bikes that can be purchased are the {2}.".format(customer.customer_name, customer.funds, customer.get_affordable_bikes))
+##for bike in bikes:
+##    s1.shop_inventory[bike.bike_name] = 20
+##print(s1.shop_inventory)
+##profit_earned = 0
+##for customer in customer_list:
+##    bike_bought = random.choice(customer.get_affordable_bikes)
+##    new_balance = customer.funds - s1.sell_price[bike_bought]
+##    print("{0} has purchased the {1}. It was sold for ${2}. That leaves {3} with ${4} left in the bicycle fund.".format(customer.customer_name, bike_bought, s1.sell_price[bike_bought], customer.customer_name, new_balance))
+##    s1.shop_inventory[bike_bought] -= 1
+##print(s1.shop_inventory)
+##print(s1.sell_price)
 
-##bike = Bicycle("Fasty", 25 , 225)
-##print(bike.bike_name)
-##print(bike.weight)
-##bike.bike_name = "Quick"
-##print(bike.bike_name)
-##bike.bike_name = "Slow-Slow"
-##print(bike.bike_name)
-
-
-shop1 = Bikeshop("Bikeland", {}, 200, 100)
-
-
-##print(shop1.shop_name)
-##shop1.shop_inventory = {b1.bike_name:20, b2.bike_name:20, b3.bike_name:20, b4.bike_name:20, b5.bike_name:20, b6.bike_name:20}
-for bike in bikes:
-    print(bike.bike_name)
-    shop1.shop_inventory = {bike.bike_name: 20}
-print(shop1.shop_inventory)
-
-
-##print(customer1.customer_name)
-##print(customer1.funds)
-##print(customer2.customer_name)
-##print(customer2.funds)
-##print(customer3.customer_name)
-##print(customer3.funds)
-##shop1.shop_inventory = "cold"
-##shop1.shop_name = 0
-##bike.bike_name = 8
-##bike.weight = "ice"
-##bike.production_cost = "fire"
-##shop1.sell_price = "Cheese"
-##shop1.profit = "cash"
-##customer1.funds = "money"
-##customer1.customer_name = 7
-customer1.buy = 10
